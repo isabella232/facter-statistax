f6128d0f5287e162a5de121efa272d80eea4e80e
Andrei Filipovici <andrei.filipovici@andreis-mbp.eth.tsr.corp.puppet.net>
2020-02-12 15:18:51 +0200

    (FACT-2293) adapted scripts for the facter name change from facter-ng to facter uncommented all plaforms from the configuration file

--- acceptance/presuite/011_install_facter_ng.rb
+++ acceptance/presuite/011_install_facter_ng.rb
@@ -3,15 +3,14 @@
 test_name 'Install facter gem' do
   agents.each do |agent|
     current_dir = Pathname.new(File.expand_path('..', __dir__))
-    facter_ng_path = Dir.entries(current_dir).select { |file| file =~ /facter-ng-[0-9]+.[0-9]+.[0-9]+.gem/ }
+    facter_ng_path = Dir.entries(current_dir).select { |file| file =~ /facter-[0-9]+.[0-9]+.[0-9]+.gem/ }
     facter_gem = File.join(current_dir, facter_ng_path)
     home_dir = on(agent, 'pwd').stdout.chop
 
     scp_to agent, facter_gem, home_dir
 
     gem_c = gem_command(agent)
-    on agent, "#{gem_c} uninstall facter-ng"
-    on agent, "#{gem_c} install -f facter-ng-*.gem"
+    on agent, "#{gem_c} install -f facter-*.gem"
     if agent['platform'] =~ /windows/
       puppetbin_path = '"/cygdrive/c/Program Files (x86)/Puppet Labs/Puppet/puppet/bin":"/cygdrive/c/Program Files/Puppet Labs/Puppet/puppet/bin"'
       on agent, %( echo 'export PATH=$PATH:#{puppetbin_path}' > /etc/bash.bashrc )
--- acceptance/presuite/03_install_facter_gem.rb
+++ acceptance/presuite/03_install_facter_gem.rb
@@ -4,9 +4,9 @@ test_name 'Install facter gem' do
   agents.each do |agent|
     gem_c = gem_command(agent)
     if ENV['GEM_VERSION']
-      on agent, "#{gem_c} install -f facter-ng -v #{ENV['GEM_VERSION']}"
+      on agent, "#{gem_c} install -f facter -v #{ENV['GEM_VERSION']}"
     else
-      on agent, "#{gem_c} install -f facter-ng"
+      on agent, "#{gem_c} install -f facter"
     end
   end
 end
